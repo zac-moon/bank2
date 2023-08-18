559b73c715512da8a91922fd35eb0e5721d6b867
lulu-moon <lulumoon2014@gmail.com>
2023-08-18 17:46:05 +0100

    fixed color changes for incorrect details

--- gui.py
+++ gui.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import socket
-from tkinter import ttk
 
 root = tk.Tk()
 root.title('ZBANK LINK - LOGIN')
@@ -12,21 +11,31 @@ port = 12346
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
 
+confLabel = tk.Label(root, text="", fg='black') 
+
 def login():
     username = usernameEntry.get()
     password = passwordEntry.get()
     print(f'{username}\n{password}')
     client_socket.send(f'login.{username}.{password}'.encode('utf-8'))
-    conf =client_socket.recv(1024)
+    conf = client_socket.recv(1024)
     conf = conf.decode('utf-8')
-    print('conf')
+    print(conf)
+    if conf == "Correct Details":
+        confLabel.config(text="Correct Details", fg='green')
+    elif conf == "Incorrect Password":
+        confLabel.config(text="Incorrect Password", fg='red')  
+    elif conf == "Account Not Found":
+        confLabel.config(text='Account Not Found', fg='red')  
+    else:
+        confLabel.config(text="Sorry, we couldn't log you in. We don't know why.", fg='red')  
 
-mainTitle = tk.Label(root,text='ZBANK LINK - LOGIN',font=('Arial',60))
-usernameLabel = tk.Label(root, text='Username :')
+mainTitle = tk.Label(root, text='ZBANK LINK - LOGIN', font=('Arial', 40))  
+usernameLabel = tk.Label(root, text='Username:')
 usernameEntry = tk.Entry(root)
-passwordLabel = tk.Label(root, text='Password :')
+passwordLabel = tk.Label(root, text='Password:')
 passwordEntry = tk.Entry(root, show="*")
-loginButton = tk.Button(root, text='Login',command=login)
+loginButton = tk.Button(root, text='Login', command=login)
 
 mainTitle.pack()
 usernameLabel.pack()
@@ -34,6 +43,6 @@ usernameEntry.pack()
 passwordLabel.pack()
 passwordEntry.pack()
 loginButton.pack()
-
+confLabel.pack()
 
 root.mainloop()
